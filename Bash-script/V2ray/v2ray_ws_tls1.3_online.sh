389b632924cfd60c9046542bb79a79c1087939dc
wowaqly <60953666+wowaqly@users.noreply.github.com>
2021-08-22 14:56:41 +0800

    Update v2ray_ws_tls1.3_online.sh

--- Bash-script/V2ray/v2ray_ws_tls1.3_online.sh
+++ Bash-script/V2ray/v2ray_ws_tls1.3_online.sh
@@ -176,6 +176,7 @@ server {
     listen 443 ssl http2;
     server_name $your_domain;
     error_page 400 = https://$your_domain;
+    add_header Strict-Transport-Security "max-age=31536000";
     location / {
         proxy_pass $pretend_url;
         #如果是代理filebrowser，需要删除以下注释
@@ -239,12 +240,13 @@ server {
     listen 443 ssl http2;
     server_name $your_domain;
     error_page 400 = https://$your_domain;
+    add_header Strict-Transport-Security "max-age=31536000";
     root /etc/nginx/html;
     index index.php index.html;
     ssl_certificate /etc/nginx/ssl/fullchain.cer; 
     ssl_certificate_key /etc/nginx/ssl/$your_domain.key;
     #TLS 版本控制
-    ssl_protocols   TLSv1 TLSv1.1 TLSv1.2 TLSv1.3;
+    ssl_protocols    TLSv1.2 TLSv1.3;
     ssl_ciphers     'TLS13-AES-256-GCM-SHA384:TLS13-CHACHA20-POLY1305-SHA256:TLS13-AES-128-GCM-SHA256:TLS13-AES-128-CCM-8-SHA256:TLS13-AES-128-CCM-SHA256:EECDH+CHACHA20:EECDH+CHACHA20-draft:EECDH+ECDSA+AES128:EECDH+aRSA+AES128:RSA+AES128:EECDH+ECDSA+AES256:EECDH+aRSA+AES256:RSA+AES256:EECDH+ECDSA+3DES:EECDH+aRSA+3DES:RSA+3DES:!MD5';
     ssl_prefer_server_ciphers   on;
     # 开启 1.3 0-RTT
